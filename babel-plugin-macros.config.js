174822ab8113bccf0608703c368e10fb55de5bf9
Kentaro Ozeki <32771324+ozekik@users.noreply.github.com>
2022-01-18 12:25:28 +0900

    fix: fix tailwind config

--- babel-plugin-macros.config.js
+++ babel-plugin-macros.config.js
@@ -2,9 +2,6 @@ module.exports = {
   twin: {
     config: 'tailwind.config.js',
     preset: 'styled-components',
-    // autoCssProp: true,
-    dataTwProp: true,
-    debugPlugins: false,
-    debug: false,
+    autoCssProp: false,
   },
 };
