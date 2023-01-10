6622bc6e29fe7ffc02d63d25988e30b8464c7979
Kentaro Ozeki <32771324+ozekik@users.noreply.github.com>
2023-01-11 00:34:52 +0900

    feat(controls): [WIP] keyboard controls

--- src/ModelView.tsx
+++ src/ModelView.tsx
@@ -5,6 +5,7 @@ import * as THREE from "three";
 
 import axios from "axios";
 import debounce from "just-debounce-it";
+// import { KeyboardKeyHold } from "hold-event";
 
 import { useAtom } from "jotai";
 import * as store from "./lib/store";
@@ -159,6 +160,77 @@ function ExtendedCameraControls({ ...props }) {
       window.removeEventListener("keyup", updateConfig);
     };
   }, [camera]);
+
+  // TODO: Key controls
+  // useEffect(() => {
+  //   if (!ref || !ref.current) return;
+
+  //   const cameraControls = ref.current;
+
+  //   console.log("controls");
+
+  //   const KEYCODE = {
+  //     W: 87,
+  //     A: 65,
+  //     S: 83,
+  //     D: 68,
+  //     ARROW_LEFT: 37,
+  //     ARROW_UP: 38,
+  //     ARROW_RIGHT: 39,
+  //     ARROW_DOWN: 40,
+  //   };
+
+  //   const moveForward = (ev: any) => {
+  //       cameraControls.forward(0.05 * ev.deltaTime, false);
+  //     },
+  //     moveBackward = (ev: any) => {
+  //       cameraControls.forward(-0.05 * ev.deltaTime, false);
+  //     },
+  //     rotateLeft = (ev: any) => {
+  //       // TODO: not working; change target instead of rotation
+  //       cameraControls.rotate(
+  //         -0.1 * THREE.MathUtils.DEG2RAD * ev.deltaTime,
+  //         0,
+  //         true,
+  //       );
+  //     },
+  //     rotateRight = (ev: any) => {
+  //       cameraControls.rotate(
+  //         0.1 * THREE.MathUtils.DEG2RAD * ev.deltaTime,
+  //         0,
+  //         true,
+  //       );
+  //     };
+
+  //   const wKey = new KeyboardKeyHold(KEYCODE.W, 16.666);
+  //   const aKey = new KeyboardKeyHold(KEYCODE.A, 16.666);
+  //   const sKey = new KeyboardKeyHold(KEYCODE.S, 16.666);
+  //   const dKey = new KeyboardKeyHold(KEYCODE.D, 16.666);
+  //   aKey.addEventListener("holding", rotateLeft);
+  //   dKey.addEventListener("holding", rotateRight);
+  //   wKey.addEventListener("holding", moveForward);
+  //   sKey.addEventListener("holding", moveBackward);
+
+  //   const leftKey = new KeyboardKeyHold(KEYCODE.ARROW_LEFT, 16.666);
+  //   const rightKey = new KeyboardKeyHold(KEYCODE.ARROW_RIGHT, 16.666);
+  //   const upKey = new KeyboardKeyHold(KEYCODE.ARROW_UP, 16.666);
+  //   const downKey = new KeyboardKeyHold(KEYCODE.ARROW_DOWN, 16.666);
+  //   leftKey.addEventListener("holding", rotateLeft);
+  //   rightKey.addEventListener("holding", rotateRight);
+  //   upKey.addEventListener("holding", moveForward);
+  //   downKey.addEventListener("holding", moveBackward);
+
+  //   return () => {
+  //     aKey.removeEventListener("holding", rotateLeft);
+  //     dKey.removeEventListener("holding", rotateRight);
+  //     wKey.removeEventListener("holding", moveForward);
+  //     sKey.removeEventListener("holding", moveBackward);
+  //     leftKey.removeEventListener("holding", rotateLeft);
+  //     rightKey.removeEventListener("holding", rotateRight);
+  //     upKey.removeEventListener("holding", moveForward);
+  //     downKey.removeEventListener("holding", moveBackward);
+  //   };
+  // }, [ref?.current]);
 
   return (
     <CameraControls
