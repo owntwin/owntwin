5a07c8af9dedb34fb808f626d07604a457a76eae
Kentaro Ozeki <32771324+ozekik@users.noreply.github.com>
2023-01-16 00:00:00 +0900

    update(sidenav): dropdown menu with radix

--- src/ui/Sidenav.tsx
+++ src/ui/Sidenav.tsx
@@ -1,4 +1,7 @@
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+
 import { mdiHelpCircleOutline, mdiAccountGroup, mdiFullscreen } from "@mdi/js";
+import { Icon } from "@mdi/react";
 
 import DiscussInput from "../addon/discuss/components/Input";
 import DiscussButton from "../addon/discuss/components/Button";
@@ -14,20 +17,68 @@ const addons = import.meta.env.VITE_ADDONS
   ? import.meta.env.VITE_ADDONS.split(",")
   : [];
 
-function Sidenav({ communityURL, ...props }: { communityURL?: string }) {
+function HelpMenuButton() {
   const HELP_URL = "https://beta.owntwin.com/docs/about";
 
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild>
+        {/* <a href={HELP_URL} target="_blank" rel="noreferrer"> */}
+        {/* <svg style={{ width: "24px", height: "24px" }} viewBox="0 0 24 24">
+            <path fill="#000000" d={mdiHelpCircleOutline} />
+          </svg> */}
+        <button className="focus-visible:outline-none">
+          <Icon
+            path={mdiHelpCircleOutline}
+            size="24px"
+            className="fill-current hover:text-gray-500"
+          />
+        </button>
+        {/* </a> */}
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content
+          className="border shadow-sm rounded-md bg-white mx-2 px-0 py-1"
+          sideOffset={5}
+        >
+          <DropdownMenu.Item asChild>
+            <a
+              href="./twin.json"
+              target="_blank"
+              rel="noopener"
+              className="block w-32 text-sm px-4 py-2 hover:bg-gray-100 focus-visible:outline-none"
+            >
+              定義ファイルを開く
+            </a>
+          </DropdownMenu.Item>
+          {/* <DropdownMenu.Item className="px-4 py-2 hover:bg-gray-100 focus-visible:outline-none">
+            ライセンス・権利情報
+          </DropdownMenu.Item> */}
+          <DropdownMenu.Item asChild>
+            <a
+              href={HELP_URL}
+              target="_blank"
+              rel="noopener"
+              className="block w-32 text-sm px-4 py-2 hover:bg-gray-100 focus-visible:outline-none"
+            >
+              ヘルプ
+            </a>
+          </DropdownMenu.Item>
+          <DropdownMenu.Arrow className="fill-current text-gray-200" />
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  );
+}
+
+function Sidenav({ communityURL, ...props }: { communityURL?: string }) {
   return (
     <div
       className="fixed bottom-4 left-4 right-4 sm:right-auto flex items-center h-10"
       style={{ zIndex: Z_INDEX.sidenav }}
     >
       <div className="flex items-center">
-        <a href={HELP_URL} target="_blank" rel="noreferrer">
-          <svg style={{ width: "24px", height: "24px" }} viewBox="0 0 24 24">
-            <path fill="#000000" d={mdiHelpCircleOutline} />
-          </svg>
-        </a>
+        <HelpMenuButton />
       </div>
       {!!communityURL && (
         <div className="ml-3 flex items-center">
