88151b85353bc437d6fb41dcf8d3dbd0e18f90dc
Kentaro Ozeki <32771324+ozekik@users.noreply.github.com>
2023-02-02 00:00:00 +0900

    chore: update build setups

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -10,13 +10,22 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: {
-        "index": resolve(__dirname, "src/index.ts"),
-        "store": resolve(__dirname, "src/store.ts"),
+        index: resolve(__dirname, "src/index.ts"),
+        store: resolve(__dirname, "src/store.ts"),
         "components/index": resolve(__dirname, "src/components/index.ts"),
-        "components/Field/hooks": resolve(__dirname, "src/components/Field/hooks.ts"),
-        "components/ModelView/store": resolve(__dirname, "src/components/ModelView/store.ts"),
+        "components/Field/hooks": resolve(
+          __dirname,
+          "src/components/Field/hooks.ts",
+        ),
+        "components/ModelView/store": resolve(
+          __dirname,
+          "src/components/ModelView/store.ts",
+        ),
         "lib/utils": resolve(__dirname, "src/lib/utils.ts"),
-        "lib/components/index": resolve(__dirname, "src/lib/components/index.ts"),
+        "lib/components/index": resolve(
+          __dirname,
+          "src/lib/components/index.ts",
+        ),
         // "components/Field/store": resolve(__dirname, "src/components/Field/store.ts"),
         // "components/Field/Field": resolve(__dirname, "src/components/Field/Field.tsx"),
         // "components/Camera/Camera": resolve(__dirname, "src/components/Camera/Camera.tsx"),
