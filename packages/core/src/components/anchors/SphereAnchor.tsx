463b4dd8c7cf3475183729dd354c43aef32a0823
Kentaro Ozeki <32771324+ozekik@users.noreply.github.com>
2023-02-09 00:00:00 +0900

    update(core): update SphereAnchor

--- packages/core/src/components/anchors/SphereAnchor.tsx
+++ packages/core/src/components/anchors/SphereAnchor.tsx
@@ -1,31 +1,37 @@
-import { Html } from "@react-three/drei";
+import { useAtom } from "jotai";
+import * as store from "../../store";
+
+import { Label } from "../labels";
 
 export function SphereAnchor({
   position,
   label,
+  labelVisibility = "auto",
+  radius = 40,
   color = 0x2196f3,
+  opacity = 0.25,
 }: {
   position: [number, number, number];
   label?: string;
+  labelVisibility?: string;
+  radius?: number;
   color?: string | number;
+  opacity?: number;
 }) {
+  const [closeup] = useAtom(store.closeupAtom);
+
   return (
     <mesh position={position}>
-      <sphereBufferGeometry args={[40, 20, 20]} />
-      <meshBasicMaterial color={color} opacity={0.5} transparent={true} />
-      <Html style={{ pointerEvents: "none", userSelect: "none" }}>
-        <div
-          style={{
-            display: true ? "block" : "none",
-            fontSize: "0.75rem",
-            fontWeight: "normal",
-            width: "10rem",
-            color: "rgb(156 163 175)",
-          }}
-        >
-          {label}
-        </div>
-      </Html>
+      <sphereGeometry args={[radius, 20, 20]} />
+      <meshBasicMaterial
+        color={color}
+        opacity={opacity}
+        transparent={true}
+        depthWrite={false}
+      />
+      <Label visible={labelVisibility === "always" || closeup}>
+        <pre>{label}</pre>
+      </Label>
     </mesh>
   );
 }
